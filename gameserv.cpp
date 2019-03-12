c78621b7555b6375968b8164d6d4c360bff02de0
Michael Hazell <michaelhazell@hotmail.com>
2019-03-12 16:01:04 -0400

    Fix an issue causing the GameServ client name to not be set on rehash

--- gameserv.cpp
+++ gameserv.cpp
@@ -19,7 +19,9 @@ class GameServCore : public Module
 	GameServCore(const Anope::string& modname, const Anope::string& creator) : Module(modname, creator, PSEUDOCLIENT | THIRD)
 	{
 		if(!IRCD)
+		{
 			throw ModuleException("IRCd protocol module not loaded");
+		}
 	}
 
 	void OnReload(Configuration::Conf* conf) anope_override
@@ -27,17 +29,25 @@ class GameServCore : public Module
 		const Anope::string& gsnick = conf->GetModule(this)->Get<const Anope::string>("client");
 
 		if(gsnick.empty())
+		{
 			throw ConfigException(Module::name + ": <client> must be defined");
+		}
 
 		BotInfo* bi = BotInfo::Find(gsnick, true);
 		if(!bi)
+		{
 			throw ConfigException(Module::name + ": no bot named " + gsnick);
+		}
+
+		GameServ = bi;
 	}
 
 	EventReturn OnPreHelp(CommandSource& source, const std::vector<Anope::string>& params) anope_override
 	{
 		if (!params.empty() || source.c || source.service != *GameServ)
+		{
 			return EVENT_CONTINUE;
+		}
 		source.Reply(_("%s commands:"), GameServ->nick.c_str());
 		return EVENT_CONTINUE;
 	}
